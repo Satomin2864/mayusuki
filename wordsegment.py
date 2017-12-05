6bbf67b5faaaee8a5b03bb5fa185b0f9b1517217
Satomin2864 <satomiyuta0602@gmail.com>
2017-12-05 15:24:06 +0900

    単語ごとに分割した物をsplite.txtとして出力

--- wordsegment.py
+++ wordsegment.py
@@ -1,7 +1,26 @@
 import MeCab
-m = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+import csv
+
+m = MeCab.Tagger('-Owakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
 
 text = '''
-アイドルマスターシンデレラガールズ
+アイドルマスターシンデレラガールズ佐久間まゆ荒木比奈堀裕子
 '''
-print(m.parse(text))
+
+fi = open('mamayu_tweet.csv', 'r')
+fo = open('split.txt', 'w')
+for tweet in fi:
+    # print(tweet)
+    print(m.parse(tweet))
+    sprit_tweet = m.parse(tweet)
+    fo.write(sprit_tweet)
+fi.close()
+fo.close()
+# print(type(text))
+# print(type(f))
+
+# print(m.parse(f))
+
+# f = open('text.txt,'w')
+# f.write()
+# f.close()
